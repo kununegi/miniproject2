f9194e6e1f781d39d65e2981e1bb38ed5a3b17e8
kundan singh <kununegi@gmail.com>
2022-12-15 12:41:36 +1300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require ("express")
+const app = express ()
+app.get("/", (req, res)=>{
+    res.send("<h1> Hello Word </h1>")
+});
+
+const port = process.env.PORT || 3000
+
+app.listen (port, ()=> console.log(`Listening on port ${port}`))
